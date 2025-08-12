4978a263d454f80e731807a92e8e415e44feec1b
Hansol202 <gksthfeld@gmail.com>
2025-08-13 01:02:58 +0900

    Add files via upload

---
+++ 1_Basics_250811/13_must_know_array_function.js
@@ -0,0 +1,47 @@
+/**
+ * Array Methods
+ */
+let iveMembers = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서',
+];
+
+console.log(iveMembers);
+
+// push()
+console.log(iveMembers.push('코딩'));
+console.log(iveMembers);
+
+console.log('----------------------');
+
+// pop()
+console.log(iveMembers.pop());
+console.log(iveMembers);
+
+console.log('----------------------');
+
+//shift()
+console.log(iveMembers.shift());
+console.log(iveMembers);
+
+// unshift()
+console.log(iveMembers.unshift('안유진'));
+console.log(iveMembers);
+
+console.log(iveMembers.splice(0, 3));
+console.log(iveMembers);
+
+iveMembers = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서',
+];
+
+console.log(iveMembers);
---
+++ 1_Basics_250811/4_naming_conventions.js
@@ -0,0 +1,18 @@
+/**
+ * naming conventions
+ * 
+ * 1) 일반적으로 영어 (라틴문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ * 4) 키워드는 변수명으로 사용 할 수 없다
+ *  외울 필요 없음 자연스럽게 습득
+ */
+
+let coding = '코딩';
+var $name = '달러';
+const _underscore = '언더스코어';
+
+console.log(coding);
+console.log($name); 
+console.log(_underscore);
+
