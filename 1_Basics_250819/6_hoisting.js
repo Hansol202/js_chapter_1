327a23d9fea92cb1b0331d58c5fc421c6842340b
Hansol202 <gksthfeld@gmail.com>
2025-08-19 21:38:17 +0900

    Add files via upload

---
+++ 1_Basics_250819/6_hoisting.js
@@ -0,0 +1,18 @@
+// console.log(name);
+// var name = '코딩';
+// console.log(name);
+
+// var name;
+// console.log(name);
+// name = '코딩';
+// console.log(name) ;
+
+/**
+ * Hoisting은 무엇인가?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것 처럼 느껴지는 현상을 이야기한다.
+ * 
+ */
+
+console.log(yuJin);
+// let yuJin = '안유진';
