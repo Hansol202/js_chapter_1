fa2ed8ccfa270f5fc69b0351799363a3d791d7f1
Hansol202 <gksthfeld@gmail.com>
2025-09-04 23:39:52 +0900

    Add files via upload

--- 3_all_about_object/5_prototype_attribute.js
+++ 3_all_about_object/5_prototype_attribute.js
@@ -73,3 +73,60 @@ console.log(wonYoung2.sayHello());
 console.log(yuJin3.sayHello === wonYoung3.sayHello);
 
 console.log(yuJin.hasOwnProperty('sayHello'));
+
+IdolModel3.sayStatcHello = function(){
+    return '안녕하세요 저는 static method 입니다.';
+}
+
+console.log(IdolModel3.sayStatcHello());
+
+/**
+ * Overriding
+ */
+function IdolModel4(name, year){
+    this.name = name;
+    this.year = year;
+
+    this.sayHello = function(){
+        return '안녕하세요 저는 인스턴스 메서드입니다.';
+    }
+}
+
+IdolModel4.prototype.sayHello = function(){
+    return '안녕하세요 저는 prototype 메서드 입니다.';
+}
+
+const yuJin4 = new IdolModel4('안유진', 2003);
+
+// 프로퍼티 셰도잉 - class에서 override
+console.log(yuJin4.sayHello());
+
+/**
+ * getPrototypeOf, setprototypeOf
+ * 
+ * 인스턴스의 --proto__ 변경 vs 함수의 prototype 변경
+ */
+function IdolModel(name, year){
+    this.name = name;
+    this.year = year;
+}
+
+IdolModel.prototype.sayHeloo = function(){
+    return `${this.name} 인사를 합니다.`;
+}
+
+function FemaleIdolModel(name, year){
+    this.name = name;
+    this.year = year;
+
+    this.dance = function(){
+        return `${this.name}이 춤을 춥니다.`;
+    }
+}
+
+const gaEul = new IdolModel('가을', 2004);
+const ray = new IdolModel('레이', 2004);
+
+console.log(gaEul.__proto__);
+console.log(gaEul.__proto__ === IdolModel.prototype);
+console.log(Object.getPrototypeOf(gaEul) === IdolModel.prototype);
