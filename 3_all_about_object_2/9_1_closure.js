bb2672f09def7e3d3610a7a898e2ea6bebbad00b
Hansol202 <gksthfeld@gmail.com>
2025-09-08 19:07:04 +0900

    Add files via upload

---
+++ 3_all_about_object_2/9_1_closure.js
@@ -0,0 +1,55 @@
+/**
+ * Closure
+ * 
+ * A closure is the combination of a function and the 
+ * lexical environment within which that function was declared.
+ * 
+ * "클로저는 어떤 함수와 해당 함수가 선언된 렉시컬 환경의 조합이다."
+ * 
+ * "상위 함수보다 하위 함수가 더 오래 살아있는 경우를 Closure라고 한다."
+ */
+function getNumber(){
+    var number = 5;
+
+    function innerGetNumber(){
+        return number;
+    }
+
+    return innerGetNumbe();
+}
+
+// console.log(number);
+
+// console.log(getNumber());
+
+function getNumber(){
+    var number = 5;
+
+    function innerGetNumber(){
+        return number;
+    }
+
+    return innerGetNumber;
+}
+
+const runner = getNumber();
+
+console.log(runner);
+console.log(runner());
+
+/**
+ * 1) 데이터 캐싱
+ */
+function cacheFunction(newNumb){
+    // 아래 계산은 매우 오래걸린다는 가정을 했을때
+    var number = 10 * 10;
+
+    function innerCacheFunction(newNumb){
+        return number * newNumb;
+    }
+
+    return number * newNumb;
+}
+
+const runner2 = cacheFunction();
+console.log(runner2(10));
